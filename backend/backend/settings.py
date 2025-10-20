dba5a89cf686c01ca83f781b2b27cfeb8da2a82f
Adika2003 <adikasultana88m@gmail.com>
2025-10-20 16:37:49 +0600

    chamge

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,12 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-
     'rest_framework',
-
     'rest_framework.authtoken',
     'djoser',
-
     "corsheaders",
 ]
 
