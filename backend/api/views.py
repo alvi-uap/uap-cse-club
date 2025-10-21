925b341d1c43ed8b49f078f0d7eb90fec9e7deaf
alvi-uap <huqalvi4@gmail.com>
2025-10-22 00:28:59 +0600

    views.py error fixing

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,6 +8,7 @@ from rest_framework import viewsets, status, generics, permissions
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser, FormParser
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     permission_classes = [IsAuthenticated]
@@ -26,85 +27,74 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get', 'put', 'patch'], parser_classes=[MultiPartParser, FormParser])
     def me(self, request):
         user_profile, created = UserProfile.objects.get_or_create(user=request.user)
-        
+
         if request.method == 'GET':
             serializer = self.get_serializer(user_profile)
             return Response(serializer.data)
-        
+
         elif request.method in ['PUT', 'PATCH']:
             serializer = UserProfileUpdateSerializer(
-                user_profile, 
-                data=request.data, 
+                user_profile,
+                data=request.data,
                 partial=(request.method == 'PATCH')
             )
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            
+
             updated_profile = UserProfile.objects.get(user=request.user)
             full_serializer = UserProfileSerializer(updated_profile)
             return Response(full_serializer.data)
 
+
 class ClubViewSet(viewsets.ModelViewSet):
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated]
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated] 
- 
+    permission_classes = [IsAuthenticated]
+
+
 class ConvenerViewSet(viewsets.ModelViewSet):
     queryset = Convener.objects.all()
     serializer_class = ConvenerSerializer
-<<<<<<< HEAD
-
-
-=======
     permission_classes = [IsAuthenticated]
->>>>>>> 047fb87aeff992ad7419a72bb967cf48b75f9625
+
 
 class PresidentViewSet(viewsets.ModelViewSet):
     queryset = President.objects.all()
     serializer_class = PresidentSerializer
     permission_classes = [IsAuthenticated]
 
+
 class VisePresidentViewSet(viewsets.ModelViewSet):
     queryset = VisePresident.objects.all()
     serializer_class = VisePresidentSerializer
     permission_classes = [IsAuthenticated]
+
 
 class GeneralSecretaryViewSet(viewsets.ModelViewSet):
     queryset = GeneralSecretary.objects.all()
     serializer_class = GeneralSecretarySerializer
     permission_classes = [IsAuthenticated]
 
+
 class TreasurerViewSet(viewsets.ModelViewSet):
     queryset = Treasurer.objects.all()
     serializer_class = TreasurerSerializer
     permission_classes = [IsAuthenticated]
+
 
 class ExecutivesViewSet(viewsets.ModelViewSet):
     queryset = Executives.objects.all()
     serializer_class = ExecutivesSerializer
     permission_classes = [IsAuthenticated]
 
+
 class EventsViewSet(viewsets.ModelViewSet):
     queryset = Events.objects.all()
     serializer_class = EventsSerializer
-<<<<<<< HEAD
     permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        if self.request.user.is_superuser:
-            return UserProfile.objects.all()
-        return UserProfile.objects.filter(user=self.request.user)
-    def get_queryset(self):
-        if self.request.user.is_superuser:
-            return UserProfile.objects.all()
-        return UserProfile.objects.filter(user=self.request.user)
-
-
-=======
-    permission_classes = [IsAuthenticated]
->>>>>>> 047fb87aeff992ad7419a72bb967cf48b75f9625
